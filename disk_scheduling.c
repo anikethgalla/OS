a8506635cbe76afbf0d28da34609ab723572f09f
anikethgalla <anikethgalla@gmail.com>
2026-04-12 10:47:47 +0530

    a

--- disk_scheduling.c
+++ disk_scheduling.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-void sort(int arr[],int n){
+void sort(int arr[], int n){
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             if(arr[i]>arr[j]){
@@ -15,30 +15,38 @@ void sort(int arr[],int n){
 
 int main(){
     int req[]={176,79,34,60,92,11,41};
-
     int n=7;
     int head=50;
     int size=200;
     int total=0;
+
     sort(req,n);
-    int start =head;
+
+    int start = head;
+
+    // service right side
     for(int i=0;i<n;i++){
-        if(req[i]>=head){
-            total+=abs(req[i]-head);
-            head=req[i];
+        if(req[i]>=start){
+            total += abs(req[i]-head);
+            head = req[i];
         }
     }
 
-    total+=abs((size-1)-head);
-    head=0;
+    // go to end
+    total += abs((size-1)-head);
+    head = size-1;
 
+    // jump to start (circular)
+    total += (size-1);   // IMPORTANT
+    head = 0;
 
+    // service left side
     for(int i=0;i<n;i++){
-        if(req[i]<start){
-            total+=abs(req[i]-head);
-            head=req[i];
+        if(req[i] < start){
+            total += abs(req[i]-head);
+            head = req[i];
         }
     }
 
-    printf("%d",total);
+    printf("Total Seek Time: %d\n", total);
 }
